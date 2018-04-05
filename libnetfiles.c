40dc465a804b054f1f069db76b836a5e9a5930a1
Matthew A. Mann <mam1010@cp.cs.rutgers.edu>
2018-04-05 17:49:24 -0400

    Basic Changes

--- libnetfiles.c
+++ libnetfiles.c
@@ -1,2 +1,28 @@
 //TODO:do stuff
 //make netopen(), netread(), netwrite(), abd netclose()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
+netserverinit(const char* hostname, const char* mode){
+	
+	return 0;
+}
+
+int netopen(const char *pathname, int flags){
+}
+
+size_t netread(int fildes, void *buf, size_t nbyte){
+}
+
+size_t netread(int fildes, void *buf, size_t nbyte){
+}
+
+int netclose(int fd){
+}
